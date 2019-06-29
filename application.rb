163c5af3fa70cb3cbfa7da8c08e710f1665743bc
David Charles <ratlover03@gmail.com>
2019-06-29 00:19:59 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Yo Whattap Earth hows' it goin'"
+    resp.write "Hello, my name is Bond, James Bond **theme plays**"
     resp.finish
   end
 
